f969c7e5a81ff71b1397d3ccd7d606a987c0ac60
Dylan N Pham <dylanpn@oxtros.dev>
2021-12-05 15:59:34 +0100

    update

--- clustering/clustering_method.py
+++ clustering/clustering_method.py
@@ -54,7 +54,7 @@ def clustering(model, input_channel, dataLoader: DataLoader, foldername: str, de
 
 
     ####### init clustered input
-    skip_const = 4
+    skip_const = 2
     D1 = len(imgs)
     D2 = int(24 / skip_const)
     D3 = int(24 / skip_const)
@@ -128,7 +128,7 @@ def clustering(model, input_channel, dataLoader: DataLoader, foldername: str, de
             start_time = time.time()
             cluster_model = dbscan(reshaped_img_enc, eps=eps)
             print("--- DBScan: %s seconds ---" % (time.time() - start_time))
-            model_name = "dbscan_4.json"
+            model_name = "dbscan_2.json"
             
             central = dbscan_central(reshaped_img_enc, cluster_model)
             path = os.path.join(os.path.abspath(os.getcwd()), "clustering/model/")
