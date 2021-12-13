3d24fe0f4d77d37f70978643ab6be5f7d9d4e624
Dylan N Pham <dylanpn@oxtros.dev>
2021-12-13 18:02:41 +0100

    update

--- run_on_cluster.sh
+++ run_on_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1            # use GPU/CUDA to speed up training (make sure your code support this)
+
 #SBATCH -c 8
-#SBATCH --constraint="avx2"
+
 
 module load nvidia/cuda-10.2
 python main.py
