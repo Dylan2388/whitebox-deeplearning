716fb655ef0d5938ba706b9a3561672747aba3c3
Dylan N Pham <dylanpn@oxtros.dev>
2021-12-17 16:39:43 +0100

    update

--- clustering/clustering_method.py
+++ clustering/clustering_method.py
@@ -189,7 +189,7 @@ def clustering(model, input_channel, dataLoader: DataLoader, foldername: str, de
     if not training:
         ### Load DBScan Model:
         model_name = "dbscan_4_0.70.json"
-        model_path = "/Users/dylan/Twente/Capita Selecta/project/clustering/model"
+        path = os.path.join(os.path.abspath(os.getcwd()), "clustering/model/")
         model = dbscan_load(os.path.join(model_path, model_name))
         ##### DECISION TREE CLASSIFIER
         embedded_vector = model["data"]
