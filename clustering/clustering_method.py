2290801e70aaec06a1c6f2a1cb19c47c4968c965
Dylan N Pham <dylanpn@oxtros.dev>
2021-12-22 16:04:42 +0100

    update

--- clustering/clustering_method.py
+++ clustering/clustering_method.py
@@ -124,7 +124,7 @@ def clustering(model, input_channel, dataLoader: DataLoader, foldername: str, de
             #### use 32-dimension as well
             #### use 16-dimension as well
             #### -->  
-            eps = 0.54
+            eps = 0.52
             start_time = time.time()
             cluster_model = dbscan(reshaped_img_enc, eps=eps)
             print("--- DBScan: %s seconds ---" % (time.time() - start_time))
